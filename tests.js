0a03cae79c42badc02bf63392bbc2cf78655f782
James Thigpen <james.r.thigpen@gmail.com>
2009-10-25 23:57:53 -0700

    New Syntax Experiment

--- tests.js
+++ tests.js
@@ -1,20 +1,6 @@
-
-var assertTrue = function(obj1, obj2) {
-	ok(obj1, obj2);
-};
-
-Object.prototype.shouldEqual = function (that) {
-	assertTrue(this, that);
-};
-
 String.prototype.startsWith = function (string) {
 	return this.indexOf(string) === 0;
 };
-
-function camelCaseToSpaces(string)
-{
-	
-}
 
 function clone(object) {
 	function F() {}
@@ -22,39 +8,32 @@ function clone(object) {
 	return new F;
 }
 
-var ContextSpecification = {
-	establishContext: function () {},
-	
-	because: function () {},
-	
-	executeTest: function (name, assertion) {
-		this.establishContext();
-		this.because();
-		var localContext = this;
-		test(name, function () localContext[assertion]());
-	},
-	
-	executeTests: function () {
-		for (property in this)
-		{
-			if (property.startsWith("itShould"))
-				this.executeTest(property, property);
-		}
-	}
+Object.prototype.shouldEqual = function (that) {
+	equals(this, that);
 };
 
-whenIAddTwoNumbers = clone(ContextSpecification);
-whenIAddTwoNumbers.name = "When I Add Two Numbers";
-whenIAddTwoNumbers.establishContext = function () {
-	this.num1 = 1;
-	this.num2 = 2;
+Object.prototype.shouldNotEqual = function (that) {
+	ok(this != that);
 };
 
-whenIAddTwoNumbers.because = function () { this.result = this.num1 + this.num2; };
-whenIAddTwoNumbers.itShouldCorrectlyAddNumbers = function () this.result.shouldEqual(this.num1 + this.num2);
+var _contextName;
 
+function when(contextName) { 
+	delete because;
+	delete context;
+	_contextName = contextName;
+}
 
-
-//when("I Add two numbers")
-//	.itShould("sum them").means(function () this.result.shouldEqual(this.num1 + this.num2))
-//	.itShould("not subtract them").means(function () this.result.shouldNotBeSubtracted());
+function should(specName, assertion) {
+	var contextName = _contextName;
+	var localContext = context;
+	var localBecause = because;
+	var localAssertion = assertion;
+	var currentContext = {};
+	
+	test("When " + contextName + " it should " + specName, function () {
+		localContext.call(currentContext);
+		localBecause.call(currentContext);
+		localAssertion.call(currentContext);
+	});
+}
